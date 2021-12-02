59599a133735c2ad7fee146798ab61a81fe2cb4a
LEANDRO COSTA FERNANDES <83041805+leandrocf79@users.noreply.github.com>
2021-12-02 20:01:14 -0300

    Create Curso C++.cpp
    Aulas do udemy com recursos disponíveis em
    https://github.com/professormarcosp/AprendaCParaGamesUE4

---
+++ Curso C++.cpp
@@ -0,0 +1,141 @@
+// PRATICANDO C e C++ com as aulas distribuidas no https://github.com/professormarcosp/AprendaCParaGamesUE4
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iomanip>
+
+// Comentario de uma linha
+/* Comentario
+de 
+varias 
+linhas*/
+
+int main(int argc, char** argv) {
+// o primeiro digito eh a cor do fundo(4) e a segunda (7) do texto
+system("color 53");	
+std::cout<<"Teste"<< std::endl;	
+std::cout
+	<<"1"<< std::endl
+	<<"  2"<<std::endl
+	<<"    3\n"<<std::endl
+	<<"       4"<<"\n"
+	<<"          5"<<
+	std::endl;
+
+std::cout<<"Eh possivel"<<std::endl<<"dar ENTER no texto"<<std::endl<<"mesmo escrevendo tudo na mesma"<<std::endl<<"linha"<< std::endl;
+	
+system("pause");
+system("color 07");
+
+//************
+
+	int numero;
+	numero = 45;
+	
+	std::cout<<"\n\n\nValor do numero:"<<numero<<std::endl;
+	std::cout<<"Tamanho na memoria da variavel numero: "<<sizeof(numero)<<" Bytes"<<std::endl;
+	std::cout<<"Endereco carregado na memoria: "<< &numero 
+	<<"\n\n\n";
+	system("PAUSE");
+
+//************
+
+int Num_Computadores=100;
+int Serie_Computadores=11111;
+
+std::cout<<"\n\n*****************************************"<<std::endl;
+std::cout<<"\tNum_Computadores: "<<Num_Computadores<<std::endl;
+std::cout<<"\tSerie_Computadores: "<<Serie_Computadores<<std::endl;
+std::cout<<"*****************************************\n\n"<<std::endl;
+system("PAUSE");
+
+//*************
+
+int Num;
+	char Caractere = 'A';
+        Num = Caractere;
+        std::cout << "\nValor do numero do caracter A na tabela ASCII: "<< Num<<"\n\n";
+ 		system("PAUSE");
+
+/* Enquanto tiver -->      int Num;
+RESPOSTA é 65, pois Número é um inteiro e neste caso vai receber o 
+valor contido na variável Caractere. Como a variável caractere contém 
+dentro dela a letra A será coletado o valor inteiro de A na tabela ASCII que é 65
+
+*** Se colocar -->     char Num;
+a RESPOSTA será a letra A
+*/
+
+//***************************************
+	if (system("cls")){
+	}else{
+system("PAUSE");}
+
+int numero1 = 55;
+float numero2 = 72.95;
+double numero3 = 34.345366575675674567544534534526357568579578908996787352314234656858706890567562;
+	std::cout<<"\n\n************************************************\n";
+	std::cout<<"int";
+	std::cout<<"\n\n\nValor do numero1: "<<numero<<std::endl;
+	std::cout<<"Tamanho na memoria da variavel numero 1:  "<<sizeof(numero1)<<" Bytes"<<std::endl;
+	std::cout<<"Endereco carregado na memoria: "<< &numero1 
+	<<"\n\n";
+	
+	std::cout<<"\n_______________________\n";
+	std::cout<<"float";
+	std::cout<<"\n\n\nValor do numero2: "<<numero2<<std::endl;
+	std::cout<<"Tamanho na memoria da variavel numero 2:  "<<sizeof(numero2)<<" Bytes"<<std::endl;
+	std::cout<<"Endereco carregado na memoria: "<< &numero2<<"\n\n";
+	
+	std::cout<<"\n_______________________\n";
+	std::cout<<"double";
+	std::cout<<"\n\n\nValor do numero3: "<<numero3<<std::endl;
+	std::cout<<"Tamanho na memoria da variavel numero 3:  "<<sizeof(numero3)<<" Bytes"<<std::endl;
+	std::cout<<"Endereco carregado na memoria: "<< &numero3<<"\n\n";
+	std::cout<<"\n************************************************\n\n";
+	system("PAUSE");
+
+
+//***************************************
+
+//#include <iomanip>
+std::cout<<"\n_______________________\n";
+std::cout<<"Para double, neste caso, foi incluido iomanip"<<std::endl;
+std::cout<<"\ndouble com include iomanip e setprecision (10)";
+
+// incluso o     std::setprecision (10) 
+	std::cout<<"\n\n\n\tValor do numero3: "<< std::setprecision (10)<<numero3<<std::endl;
+	std::cout<<"\tTamanho na memoria da variavel numero 3:  "<<sizeof(numero3)<<" Bytes"<<std::endl;
+	std::cout<<"\tEndereco carregado na memoria: "<< &numero3<<"\n\n";
+	std::cout<<"\n************************************************\n\n";
+system("PAUSE");
+	if (system("cls")){
+	}else{system("PAUSE");}
+
+//****************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
+
+
+
+
+
