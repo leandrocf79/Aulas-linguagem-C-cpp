87603becd2c25153fdefc24be91a03bc0f4a99aa
LEANDRO COSTA FERNANDES <83041805+leandrocf79@users.noreply.github.com>
2021-12-16 20:33:06 -0300

    Add files via upload

---
+++ biblioteka.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "biblioteka.h"
+
+int main() {
+int Num01 = 13;
+int Num02 = 22;
+std::cout<<soma(Num01,Num02) << "\n";
+system("PAUSE");
+return 0;
+}
+
+int soma (int a, int b){
+		return a+b;}
+
+
---
+++ biblioteka.h
@@ -0,0 +1,2 @@
+//lista de protótipos
+int soma(int a, int b);
