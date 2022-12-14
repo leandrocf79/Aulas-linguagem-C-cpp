0f089496a89722592b01021ceb54a6089294096a
LEANDRO CF <83041805+leandrocf79@users.noreply.github.com>
2022-12-14 20:47:07 -0300

    Update Funcoes.cpp

--- Funcoes.cpp
+++ Funcoes.cpp
@@ -32,6 +32,34 @@ int main() {
 }
 
 
+----- ou
+
+#include <iostream>
+
+int fibonacci(int n)
+{
+    int a = 0, b = 1, c, i;
+    if (n == 0) return a;
+    for (i = 2; i <= n; i++)
+    {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
+int main()
+{
+    int n = 10;
+    std::cout << "Fibonacci number at position " << n << " is " << fibonacci(n) << std::endl;
+    return 0;
+}
+
+
+
+
+
 _________________________________
 	
 Liguagem C#
